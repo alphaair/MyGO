e1f6d3a5325eb643487f0c143a281968fab00699
alphaair <alphaair@163.com>
2016-03-23 11:43:54 +0800

    标准输入演示
    nomsg

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 import "gostudy"
+import (
+    "bufio"
+    "log"
+    "os"
+)
 
 func main(){
     fmt.Println("Hello World,您好，世界!")
-    
     gostudy.Variable()
     
+    reader:=bufio.NewReader(os.Stdin)
+    key, _, _ :=reader.ReadLine()
+    log.Println("command",key)
 }
