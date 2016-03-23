a260efe5fb2be65efda86dbf8783e3d5432739ed
Alphaair <alphaair@163.com>
2016-03-24 00:23:13 +0800

    修正语法格式

--- src/gostudy/basesynctax.go
+++ src/gostudy/basesynctax.go
@@ -1,16 +1,19 @@
-// GO基础语法学习
-// alphaair 2016-03-21
+// Copyright alphaair 2016
+// 这是一个开源学习代码文件
 
+/*
+Pakcage gostudy 是一个GO基础学习包，供应学习之用。
+*/
 package gostudy
 
 import "fmt"
 
 // Variable 函数是，GO变量语法
-func  Variable() {
-    
-    var num0 int = 3
-    num1 := 3
-       
-    fmt.Printf("n1=%d,n2=%d", num0, num1);
-}
+// value.无返回值
+func Variable() {
 
+	var num0 int = 3
+	num1 := 3
+
+	fmt.Printf("n1=%d,n2=%d", num0, num1)
+}
