1fe08bcfea15c1d4027462708151ad0ec7eef705
Alphaair <alphaair@163.com>
2016-03-24 00:24:02 +0800

    格式化排版

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 import "gostudy"
 import (
-    "bufio"
-    "log"
-    "os"
+	"bufio"
+	"log"
+	"os"
 )
 
-func main(){
-    fmt.Println("Hello World,您好，世界!")
-    gostudy.Variable()
-    
-    reader:=bufio.NewReader(os.Stdin)
-    key, _, _ :=reader.ReadLine()
-    log.Println("command",key)
+func main() {
+	fmt.Println("Hello World,您好，世界!")
+	gostudy.Variable()
+
+	reader := bufio.NewReader(os.Stdin)
+	key, _, _ := reader.ReadLine()
+	log.Println("command", key)
 }
