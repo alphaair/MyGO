1a73ed214c57217d39506764fd5521120a81f18f
Alphaair <alphaair@163.com>
2016-03-30 17:13:03 +0800

    首次封闭键盘输入

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import "fmt"
-import "gostudy"
-import (
-	"bufio"
-	"log"
-	"os"
-)
+import "deltatch/common"
 
 func main() {
 	fmt.Println("Hello World,您好，世界!")
-	gostudy.Variable()
+	//gostudy.Variable()
 
-	reader := bufio.NewReader(os.Stdin)
-	key, _, _ := reader.ReadLine()
-	log.Println("command", key)
+	line := common.ReadLine("请输入一行字符：")
+	//fmt.Printf("keyborad input:%v", line)
+    fmt.Println(line)
+    line = common.ReadLine("即将终止运行...")
 }
---
+++ src/deltatch/common/saber.go
@@ -0,0 +1,30 @@
+package common
+
+import "fmt"
+import "bufio"
+import "os"
+
+// ReadKey 从当前标准输入流中读取一个字节
+func ReadKey(msg string) byte {
+    
+    if len(msg) > 0 {
+        fmt.Printf(msg)
+    }
+    
+    render := bufio.NewReader(os.Stdin)
+    key, _ := render.ReadByte()
+    
+    return key
+}
+
+// ReadLine 从当前OS标准流中读取一行字符
+func ReadLine(msg string) string {
+    if len(msg) > 0 {
+        fmt.Printf(msg)
+    }
+    
+    render := bufio.NewReader(os.Stdin)
+    buffer, _, _ := render.ReadLine()
+       
+    return string(buffer)
+}
